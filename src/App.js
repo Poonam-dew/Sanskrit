0beef78cc0b01224f4287724b257fa6b7ad9efa6
Anjalika Singh <Anjalika Singh>
2025-03-29 18:32:17 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar.js';
 import Footer from './components/Footer.js';
-import FirstPage from './Pages/FirstPage.js';
-import Home from './Pages/Home.js';
+import Home from './components/Home.jsx';
 import Login from './Pages/Login.js';
 import Event from './Pages/Event.js';
 import LearningSanskrit from './Pages/LearningSanskrit.js';
@@ -20,24 +18,19 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar/>
-        <Routes>
-        <Route path="/" element={<FirstPage/>}/>     
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>      
-        <Route path="/events" element={<Event/>}/> 
-        <Route path="/learningSanskrit" element={<LearningSanskrit/>}/>
-        <Route path="courses" element={<Courses/>}/> 
-        <Route path="/resources" element={<Resources/>}/>  
-        <Route path="/Team" element={<Team/>}/>
-
-        
-        
+        <Routes>       
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login/>} />      
+          <Route path="/events" element={<Event/>} /> 
+          <Route path="/learningSanskrit" element={<LearningSanskrit/>} />
+          <Route path="/courses" element={<Courses/>} /> 
+          <Route path="/resources" element={<Resources/>} />  
+          <Route path="/team" element={<Team/>} />
         </Routes>
         <ToastContainer />
         <Footer/>         
       </BrowserRouter>
-
-          
     </div>
   );
 }
